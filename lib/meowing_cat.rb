0b7722abcad57b50fbf2107e996cce1b17114732
Angela Appiah <alegnalawn2@yahoo.com>
2020-01-29 22:24:01 +0000

    Automatically backed up by Learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,12 +1,7 @@
-## code your solution here. 
+
 class Cat
-  def Cat=(name, sound)
-@name= name
-@sound= sound
-name= Garfield
-sound= meaw
-attr_reader :Garfield
-attr_writer :meaw
-attr_accessor :name, :sound
-puts "#{name}"."#{sound}"
+  attr_accessor :name
+  def meow
+puts "meow!"
   end
+end
